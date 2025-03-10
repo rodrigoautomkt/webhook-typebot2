06e103cf55f5358fd332608865913fa0f7e080f4
rodrigoautomkt <rodrigoautomkt@gmail.com>
2025-03-10 10:20:39 -0300

    Adicionando webhook.js e vercel.json

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Método não permitido" });
-  }
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Método não permitido" });
+    }
 
-  res.status(200).json({ message: "Webhook funcionando corretamente!" });
+    return res.status(200).json({ message: "Webhook funcionando corretamente!" });
 }
