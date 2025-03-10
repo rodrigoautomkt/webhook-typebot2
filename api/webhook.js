3b060a99f0582a2f4829ea99cbf5d4fb42edd635
rodrigoautomkt <rodrigoautomkt@gmail.com>
2025-03-10 11:11:29 -0300

    Correção do webhook e estrutura de pastas

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,10 @@
 export default function handler(req, res) {
-    if (req.method === "POST") {
-        return res.status(200).json({ message: "Webhook recebido com sucesso!" });
-    }
+  if (req.method === "POST") {
+    res.status(200).json({
+      message: "Webhook recebido com sucesso!",
+      data: req.body
+    });
+  } else {
     res.status(405).json({ error: "Método não permitido" });
+  }
 }
